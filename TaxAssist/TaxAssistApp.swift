ae71d202f9ae0a0567088c7b9b6da879ac0e5822
Dhruv Patel <dhruvthakorpatel05@gmail.com>
2026-07-01 10:22:41 -0500

    Initial Commit

---
+++ TaxAssist/TaxAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxAssistApp.swift
+//  TaxAssist
+//
+//  Created by SandboxLab on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TaxAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
