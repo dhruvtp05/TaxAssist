8fd0f39bd61c0923c5a5d10b0dbab904e15860e2
giantsrbig <giantsrbig@gmail.com>
2026-07-06 09:48:06 -0500

    HomeScreen and BottomTabBar

---
+++ TaxAssist/BottomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  BottomTabBar.swift
+//  TaxAssist
+//
+//  Created by SandboxLab on 7/6/26.
+//
+
+import SwiftUI
+
+struct BottomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomTabBar()
+}
