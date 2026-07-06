1d9099d04c301fd063672829b6d5a6eaf771899c
Dhruv Patel <dhruvthakorpatel05@gmail.com>
2026-07-06 09:59:02 -0500

    terms of service screen

---
+++ TaxAssist/TermsOfServiceScreen.swift
@@ -0,0 +1,18 @@
+//
+//  TermsOfServiceScreen.swift
+//  TaxAssist
+//
+//  Created by SandboxLab on 7/6/26.
+//
+
+import SwiftUI
+
+struct TermsOfServiceScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsOfServiceScreen()
+}
