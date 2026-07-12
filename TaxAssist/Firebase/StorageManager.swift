d85fa2513b27dfd3b1d46f7e019e5ce9af530e19
Dhruv Patel <dhruvthakorpatel05@gmail.com>
2026-07-12 12:31:14 -0500

    tons of changes

---
+++ TaxAssist/Firebase/StorageManager.swift
@@ -0,0 +1,18 @@
+//
+//  StorageManager.swift
+//  TaxAssist
+//
+//  Created by SandboxLab on 7/9/26.
+//
+
+import SwiftUI
+
+struct StorageManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StorageManager()
+}
