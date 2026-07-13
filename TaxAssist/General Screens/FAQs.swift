d755ab8c9abb18f199236be478c072c673f3dd93
su672 <dc3405539@gmail.com>
2026-07-13 11:28:19 -0500

    Added FAQ and also a transition from the home screen

---
+++ TaxAssist/General Screens/FAQs.swift
@@ -0,0 +1,18 @@
+//
+//  FAQs.swift
+//  TaxAssist
+//
+//  Created by SandboxLab on 7/13/26.
+//
+
+import SwiftUI
+
+struct FAQs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FAQs()
+}
